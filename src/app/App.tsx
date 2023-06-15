f8de0d97c9ebc8e49d010f0e24fd9dc9ee617572
nikita <nikskozlov@gmail.com>
2023-06-15 16:06:41 +0300

    The first widget (Navbar) and the first ui component for ui-kit (AppLink) were developed, for which several themes were added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import './styles/index.scss'
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Home </Link>
-            <Link to={'/about'}>About site</Link>
+            <Navbar />
             <AppRouter />
+            <button onClick={toggleTheme}>TOGGLE</button>
         </div>
     );
 };

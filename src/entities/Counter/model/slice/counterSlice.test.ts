59525ed38bbcdc45900bc4190d3d5cb867f63312
nikita <nikskozlov@gmail.com>
2023-08-24 15:44:15 +0300

    add redux store, add first entity

---
+++ src/entities/Counter/model/slice/counterSlice.test.ts
@@ -0,0 +1,19 @@
+import { CounterSchema } from '../types/counterSchema';
+import { counterActions, counterReducer } from './counterSlice';
+
+describe('counterSlice', () => {
+    test('increment counter value', () => {
+        const state: CounterSchema = { value: 10 };
+        expect(counterReducer(state, counterActions.increment()))
+            .toEqual({ value: 11 });
+    });
+    test('decrement counter value', () => {
+        const state: CounterSchema = { value: 10 };
+        expect(counterReducer(state, counterActions.decrement()))
+            .toEqual({ value: 9 });
+    });
+    test('should work with empty state', () => {
+        expect(counterReducer(undefined, counterActions.increment()))
+            .toEqual({ value: 1 });
+    });
+});

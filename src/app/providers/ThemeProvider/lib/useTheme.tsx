d1b19b3687d9adffdc47c2f69056e01d17a6383a
nikita <nikskozlov@gmail.com>
2023-08-31 16:03:15 +0300

    fix theme classes

--- src/app/providers/ThemeProvider/lib/useTheme.tsx
+++ src/app/providers/ThemeProvider/lib/useTheme.tsx
@@ -15,5 +15,7 @@ export function useTheme(): UseThemeResult {
         localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
     };
 
+    document.body.className = theme;
+
     return { theme, toggleTheme };
 }

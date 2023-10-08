daf52e84e436a925792b19f713caded96198c381
nikita <nikskozlov@gmail.com>
2023-10-08 20:39:48 +0300

    Delete input from main page

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,25 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
-// import { Counter } from 'entities/Counter';
-import { Input } from 'shared/ui/Input/Input';
-import { Counter } from '../../../entities/Counter/ui/Counter';
 
 const MainPage = () => {
     const { t } = useTranslation('home');
-    const [value, setValue] = useState('');
-
-    const onChange = (val: string) => {
-        setValue(val);
-    };
 
     return (
         <div>
             {t('Home')}
-            <Input
-                placeholder="Введите текст"
-                value={value}
-                onChange={onChange}
-            />
         </div>
     );
 };
